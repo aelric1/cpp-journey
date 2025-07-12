d2e7aca9c3a37a019b5fdb96451133c00bdb73fe
Mehdi <180872863+entlv@users.noreply.github.com>
2025-07-12 20:40:36 +0100

    Add files via upload

---
+++ abs function.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{     
+    // abs  math function algorithm : 
+    cout << "enter a number you wanna abs it :  " ;
+     int num ; 
+     cin >> num ; 
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
